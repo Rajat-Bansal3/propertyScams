3f388536215743e7a852255142f042bd93d8753c
rajat bansal <rajatbansal28082003@gmail.com>
2024-01-16 03:50:21 +0530

    database initial commit

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.Mongo)
+  .then(() => {
+    console.log("connected ");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(3000 , ()=>{
-    console.log("listening on port 3000")
+app.listen(3000, () => {
+  console.log("listening on port 3000");
 });
